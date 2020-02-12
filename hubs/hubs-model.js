272988259cc9120cc1adf14d11b353fd4a9e900f
Devanee Reid <devaneereid@gmail.com>
2020-02-12 09:10:05 -0700

    guided project

--- hubs/hubs-model.js
+++ hubs/hubs-model.js
@@ -1,5 +1,5 @@
-const knex = require('knex');
-const config = require('../knexfile.js');
+const knex = require("knex");
+const config = require("../knexfile.js");
 const db = knex(config.development);
 
 module.exports = {
@@ -14,57 +14,63 @@ module.exports = {
 };
 
 function find(query) {
-  const { page = 1, limit = 2, sortby = 'id', sortdir = 'asc' } = query;
+  const {
+    page = 1,
+    limit = 2,
+    sortby = "id",
+    sortdir = "asc",
+    skip = 0,
+  } = query;
   const offset = limit * (page - 1);
 
-  let rows = db('hubs')
+  let rows = db("hubs")
     .orderBy(sortby, sortdir)
     .limit(limit)
-    .offset(offset);
+    .offset(skip);
 
   return rows;
 }
 
 function findById(id) {
-  return db('hubs')
+  return db("hubs")
     .where({ id })
     .first();
 }
 
 async function add(hub) {
-  const [id] = await db('hubs').insert(hub);
+  const [id] = await db("hubs").insert(hub);
 
   return findById(id);
 }
 
 function remove(id) {
-  return db('hubs')
+  return db("hubs")
     .where({ id })
     .del();
 }
 
 function update(id, changes) {
-  return db('hubs')
+  return db("hubs")
     .where({ id })
-    .update(changes, '*');
+    .update(changes, "*");
 }
 
 function findHubMessages(hubId) {
-  return db('messages as m')
-    .join('hubs as h', 'm.hub_id', 'h.id')
-    .select('m.id', 'm.text', 'm.sender', 'h.id as hubId', 'h.name as hub')
+  return db("messages as m")
+    .join("hubs as h", "m.hub_id", "h.id")
+    .select("m.id", "m.text", "m.sender", "h.id as hubId", "h.name as hub")
     .where({ hub_id: hubId });
 }
 
 // You Do
 function findMessageById(id) {
-  return db('messages')
+  return db("messages")
     .where({ id })
     .first();
 }
 
 async function addMessage(message) {
-  const [id] = await db('messages').insert(message);
+  const [id] = await db("messages").insert(message);
 
   return findMessageById(id);
 }
